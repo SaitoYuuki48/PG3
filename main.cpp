bf895d82f61629b2c9c4c9d3a0beb43dd080d960
SaitoYuuki48 <115055003+SaitoYuuki48@users.noreply.github.com>
2024-01-23 11:10:43 +0900

    06_02

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include<stdio.h>
+#include<thread>
+
+void Thread1() {
+	printf("thread1\n");
+}
+
+void Thread2() {
+	printf("thread2\n");
+}
+
+void Thread3() {
+	printf("thread3\n");
+}
 
 int main() {
+	std::thread th1(Thread1);
+	std::thread th2(Thread2);
+	std::thread th3(Thread3);
+
+	th1.join();
+	th2.join();
+	th3.join();
 
 	return 0;
 }
